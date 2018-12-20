dee19cc0f2ef937bf7d4d8e37d7021997c07a073
Arley HR <arley.hr@hotmail.com>
2018-12-20 14:40:43 -0500

    setup eslint + flow

--- App.js
+++ App.js
@@ -1,31 +1,6 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Hello World!</Text>
-      </View>
-    );
-  }
-}
+// @flow
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 
 const styles = StyleSheet.create({
   container: {
@@ -39,12 +14,17 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     margin: 10,
     textShadowColor: 'lightskyblue',
-    textShadowOffset: {width: -1, height: 1},
-    textShadowRadius: 3
+    textShadowOffset: { width: -1, height: 1 },
+    textShadowRadius: 3,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+})
+
+function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.welcome}>Hello World!</Text>
+    </View>
+  )
+}
+
+export default App
