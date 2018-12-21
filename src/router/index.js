83e736984de6c95bc5673e417d8a3dba7d72c95f
Arley HR <arley.hr@hotmail.com>
2018-12-21 10:26:01 -0500

    tabs icons

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-
+import Icon from 'react-native-vector-icons/Feather'
 import { Scene, Router, Modal, Tabs, Lightbox, ActionConst } from 'react-native-router-flux'
 
 import Login from '../screens/Login'
@@ -9,7 +9,15 @@ import routerKeys from './keys'
 
 import styles from './styles'
 
-const { MAIN_TABS, LOGIN_SCREEN, HOME_SCREEN } = routerKeys
+const {
+  MAIN_TABS,
+  LOGIN_SCREEN,
+  HOME_SCREEN,
+  SCHEDULE_SCREEN,
+  ADD_SCREEN,
+  NOTIFICATIONS_SCREEN,
+  PROFILE_SCREEN,
+} = routerKeys
 
 class Navigation extends Component {
   componentDidMount() {}
@@ -23,8 +31,32 @@ class Navigation extends Component {
             {/* // MODALS */}
 
             <Scene key={MAIN_TABS} hideNavBar type={ActionConst.REPLACE}>
-              <Tabs swipeEnabled>
-                <Scene key={HOME_SCREEN} component={Home} />
+              <Tabs showLabel={false} swipeEnabled>
+                <Scene
+                  key={HOME_SCREEN}
+                  component={Home}
+                  icon={({ tintColor }) => <Icon name="list" size={24} color={tintColor} />}
+                />
+                <Scene
+                  key={SCHEDULE_SCREEN}
+                  component={Home}
+                  icon={({ tintColor }) => <Icon name="clock" size={24} color={tintColor} />}
+                />
+                <Scene
+                  key={ADD_SCREEN}
+                  component={Home}
+                  icon={({ tintColor }) => <Icon name="clock" size={24} color={tintColor} />}
+                />
+                <Scene
+                  key={NOTIFICATIONS_SCREEN}
+                  component={Home}
+                  icon={({ tintColor }) => <Icon name="bell" size={24} color={tintColor} />}
+                />
+                <Scene
+                  key={PROFILE_SCREEN}
+                  component={Home}
+                  icon={({ tintColor }) => <Icon name="user" size={24} color={tintColor} />}
+                />
               </Tabs>
             </Scene>
           </Modal>
--- src/router/keys.js
+++ src/router/keys.js
@@ -1,5 +1,13 @@
 import { arrayToKeys } from '../libs'
 
-const keys = ['LOGIN_SCREEN', 'MAIN_TABS', 'HOME_SCREEN']
+const keys = [
+  'LOGIN_SCREEN',
+  'MAIN_TABS',
+  'HOME_SCREEN',
+  'SCHEDULE_SCREEN',
+  'ADD_SCREEN',
+  'NOTIFICATIONS_SCREEN',
+  'PROFILE_SCREEN',
+]
 
 export default arrayToKeys(keys)
