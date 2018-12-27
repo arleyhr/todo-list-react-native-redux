11405d196aa85232116bee6fde4ccf8971c9bf3e
Arley HR <arley.hr@hotmail.com>
2018-12-27 11:52:44 -0500

    firebase native android configuration

--- android/app/src/main/java/com/ahrtodorn/MainActivity.java
+++ android/app/src/main/java/com/ahrtodorn/MainActivity.java
@@ -1,4 +1,4 @@
-package com.todorn;
+package com.ahrtodorn;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "todorn";
+        return "ahrtodorn";
     }
 }
--- ios/ahrtodornTests/ahrtodornTests.m
+++ ios/ahrtodornTests/ahrtodornTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface todornTests : XCTestCase
+@interface ahrtodornTests : XCTestCase
 
 @end
 
-@implementation todornTests
+@implementation ahrtodornTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
