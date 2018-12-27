46e50823879fe5027dacf32164d34493e6bab17f
Arley HR <arley.hr@hotmail.com>
2018-12-27 14:35:59 -0500

    fix android firebase native

--- android/app/src/main/java/com/ahrtodorn/MainApplication.java
+++ android/app/src/main/java/com/ahrtodorn/MainApplication.java
@@ -26,9 +26,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNFirebasePackage(),
-            new VectorIconsPackage(),
-            new RNFirebasePackage()
+          new RNFirebasePackage(),
+          new VectorIconsPackage()
       );
     }
 
